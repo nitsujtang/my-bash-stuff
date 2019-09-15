95c1ccafd95ba3d00e03bf93bd9e6cd0c81e7659
nitsujtang <justinqountang@gmail.com>
2019-09-15 16:48:53 -0700

    changing some bugs in install script

--- install.sh
+++ install.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
-sudo apt-get install cowsay >> /dev/null
 
-if [ -f "~/.bashrc" ] ; then
+if [ -f ~/.bashrc ] ; then
     echo "saving old .bashrc"
-    rm ~/.bashrc.bak
+
+    if [ -f ~/.bashrc.bak ] ; then
+        rm ~/.bashrc.bak
+    fi
+
     mv ~/.bashrc ~/.bashrc.bak
 
     echo "copying over new .bashrc"
     cp ./.bashrc ~
+else
+    echo "copying over new .bashrc"
+    cp ./.bashrc ~
 fi
 
-if [ -f "~/.bash_aliases" ] ; then
+if [ -f ~/.bash_aliases ] ; then
     echo "saving old .bash_aliases"
-    rm ~/.bash_aliases.bak
+
+    if [ -f ~/.bash_aliases.bak ] ; then
+        rm ~/.bash_aliases.bak
+    fi
+
     mv ~/.bash_aliases ~/.bash_aliases.bak
 
+    echo "copying over new .bash_aliases"
+    cp ./.bash_aliases ~
+else
     echo "copying over new .bash_aliases"
     cp ./.bash_aliases ~
 fi
 
 echo "DONE"
+source ~/.bashrc
